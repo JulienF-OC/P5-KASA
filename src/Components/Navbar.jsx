088d153011999e1b22c80ba5366368a28fad3d77
JulienF-OC <jucamlau16@gmail.com>
2025-12-05 20:51:33 +0100

    Ajout des éléments a dans nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Navbar.scss";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar_logo">
+        <img src="/logo.png" alt="Logo" />
+      </div>
+      <a href="./index.html" className="accueil active">
+        Accueil
+      </a>
+      <a href="./A-propos.html" className="a-propos">
+        A Propos
+      </a>
+    </nav>
+  );
 }
 
 export default Navbar;
