5ceb979f58f3d8740b543010e3c449503bc6f49e
JulienF-OC <jucamlau16@gmail.com>
2025-12-06 04:25:33 +0100

    Ajout de la bannière avec son scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar.jsx";
+import Banner from "./Components/Banner.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 }
