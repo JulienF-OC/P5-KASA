8f42200836db37b989bb38a502473f436f7dc04e
JulienF-OC <jucamlau16@gmail.com>
2025-12-06 17:47:56 +0100

    Ajout du fichier JSON

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import "./Content.scss";
+import apartments from "../data/logements.json";
 
 function Content() {
   return (
     <div className="container">
       <div className="cards">
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
-        <div className="card">
-          <div className="card-content"></div>
-        </div>
+        {apartments.map((apt) => (
+          <div className="card" key={apt.id}>
+            <div className="card-image">
+              <img src={apt.cover} alt={apt.title} />
+            </div>
+            <div className="card-content">
+              <h3>{apt.title}</h3>
+              <p>{apt.location}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
