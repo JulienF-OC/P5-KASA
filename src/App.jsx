92cae0c2ef6321ff04adf02482885a7558923417
JulienF-OC <jucamlau16@gmail.com>
2025-12-06 16:21:05 +0100

    Ajout des premiers élément du content principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar.jsx";
 import Banner from "./Components/Banner.jsx";
+import Content from "./Components/Content.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Content />
     </div>
   );
 }
---
+++ src/Components/Content.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Content.scss";
+
+function Content() {
+  return (
+    <div className="container">
+      <div className="cards">
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
