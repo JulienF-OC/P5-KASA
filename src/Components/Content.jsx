948d2dc49defba90ad74c50e68448851566e1a45
JulienF-OC <jucamlau16@gmail.com>
2025-12-08 00:22:48 +0100

    Ajout de la page pour les logements et le scss

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Content.scss";
 import apartments from "../data/logements.json";
 
@@ -7,10 +8,12 @@ function Content() {
     <div className="container">
       <div className="cards">
         {apartments.map((apt) => (
-          <div className="card" key={apt.id}>
-            <img src={apt.cover} alt={apt.title} />
-            <h3 className="card-title">{apt.title}</h3>
-          </div>
+          <Link to={`/logement/${apt.id}`} key={apt.id} className="card-link">
+            <div className="card">
+              <img src={apt.cover} alt={apt.title} />
+              <h3 className="card-title">{apt.title}</h3>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
