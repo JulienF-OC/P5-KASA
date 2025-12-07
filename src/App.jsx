e1ca4064e630c7268b469e00103be015d47f86cf
JulienF-OC <jucamlau16@gmail.com>
2025-12-07 22:00:35 +0100

    Ajout des routes correctement

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import React from "react";
-import Navbar from "./Components/Navbar.jsx";
-import Banner from "./Components/Banner.jsx";
-import Content from "./Components/Content.jsx";
-import Footer from "./Components/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+import Apropos from "./Components/Apropos";
+import Logement from "./Components/Logement";
+import Error404 from "./Components/Error404";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Banner />
-      <Content />
+
+      <Routes>
+        {/* PAGE D’ACCUEIL */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Content />
+            </>
+          }
+        />
+
+        {/* PAGE À PROPOS */}
+        <Route path="/a-propos" element={<Apropos />} />
+
+        {/* PAGE LOGEMENT */}
+        <Route path="/logement/:id" element={<Logement />} />
+
+        {/* PAGE 404 */}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/a-propos.html",
-    element: <A-propos />,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
